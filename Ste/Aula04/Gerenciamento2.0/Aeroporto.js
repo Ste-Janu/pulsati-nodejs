f435d217a024615d9522f6c59ac442b0a06314cc
Ste-Janu <122880008+Ste-Janu@users.noreply.github.com>
2023-02-15 09:19:42 -0300

    atividade servidor

---
+++ Ste/Aula04/Gerenciamento2.0/Aeroporto.js
@@ -0,0 +1,34 @@
+class Aeroporto {
+    cod;
+    nome;
+    endereco;
+    constructor(cod, nome, endereco) {
+        this.cod = cod;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+    
+    static addAeroporto(cod, nome,endereco){
+        let add = new Aeroporto(cod, nome, endereco);
+        return add;
+    }
+    getAeroportoNome() {
+        return `${this.cod} ${this.nome} ${this.endereco}`;
+  }
+} 
+const fs = require('fs');
+    exports.salvarAeroporto = (nomeArquivo, texto) => {
+        const promessa = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            fs.writeFile(nomeArquivo, texto, (erro) => {
+              if (erro) {
+                reject(erro);
+              } else {
+                resolve();
+              }
+            });
+          }, Math.random(3));
+        });
+        return promessa;
+      }
+exports.Aeroporto=Aeroporto;
